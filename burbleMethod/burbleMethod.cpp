e4bd344294a52c5d354a29cfee0e0c282866c9d9
AlexChavezz <alexischavez1234.ac@gmail.com>
2022-09-29 21:35:07 -0500

    burble method aded

---
+++ burbleMethod/burbleMethod.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector <int> burbleMethod(vector <int> &v)
+{
+    int temp;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v.size() - 1; j++)
+        {
+            if (v[j] > v[j + 1])
+            {
+                temp = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = temp;
+            }
+        }
+    }
+    return v;
+}
+
+void printVector(vector <int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector <int> v = { 5, 4, 3, 2, 1 };
+    vector orderArray = burbleMethod(v);
+    printVector(orderArray);
+    return 0;
+}
